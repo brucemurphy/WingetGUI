2ecac21ffd2315a889a038baf78bb90a1ff32cf0
brucemurphy <64602550+brucemurphy@users.noreply.github.com>
2023-03-24 09:50:01 -0700

    Add files via upload

---
+++ bin/Debug/net6.0-windows/Sample.cmd
@@ -0,0 +1,81 @@
+﻿ 
+@Echo Off
+CLS
+Echo =========================================================
+Echo Winget Install - Adobe Acrobat Reader DC (64-bit)
+Echo =========================================================
+WINGET Install "Adobe.Acrobat.Reader.64-bit" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Alexa                       
+Echo =========================================================
+WINGET Install "9N12Z3CCTCNZ" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Audacity
+Echo =========================================================
+WINGET Install "Audacity.Audacity" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - iTunes       
+Echo =========================================================
+WINGET Install "Apple.iTunes" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Bluetooth Audio Receiver
+Echo =========================================================
+WINGET Install "9N9WCLWDQS5J" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Windows Assessment and Deployment Kit              
+Echo =========================================================
+WINGET Install "Microsoft.WindowsADK" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Windows Assessment and Deployment Kit Windows Prei…
+Echo =========================================================
+WINGET Install "Microsoft.ADKPEAddon" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Microsoft Mouse and Keyboard Center
+Echo =========================================================
+WINGET Install "Microsoft.MouseandKeyboardCenter" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Surface                                 
+Echo =========================================================
+WINGET Install "9WZDNCRFJB8P" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Microsoft Garage Mouse without Borders
+Echo =========================================================
+WINGET Install "Microsoft.MouseWithoutBorders" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Microsoft Remote Desktop                    
+Echo =========================================================
+WINGET Install "9WZDNCRFJ3PS" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Spotify   
+Echo =========================================================
+WINGET Install "Spotify.Spotify" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Sysinternals Suite               
+Echo =========================================================
+WINGET Install "9P7KNL5RWT25" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - My Dell                                    
+Echo =========================================================
+WINGET Install "9PN7T3JFPRZ6" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - WeChat        
+Echo =========================================================
+WINGET Install "Tencent.WeChat" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - WhatsApp               
+Echo =========================================================
+WINGET Install "9NKSQGP7F2NH" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Zoom - One Platform to Connect   
+Echo =========================================================
+WINGET Install "XP99J3KP4XZ4VV" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Microsoft Teams                      
+Echo =========================================================
+WINGET Install "Microsoft.Teams" --accept-package-agreements
+Echo =========================================================
+Echo Winget Install - Microsoft 365 Apps for enterprise            
+Echo =========================================================
+WINGET Install "Microsoft.Office" --accept-package-agreements
+Del C:\Users\Public\*.lnk /Q /S
+Del %USERS%\Desktop\*.lnk /Q /S
