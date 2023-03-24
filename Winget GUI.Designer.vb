b186906ed023d654897f43bab1bf9a1314d0f6d3
brucemurphy <64602550+brucemurphy@users.noreply.github.com>
2023-03-24 09:00:32 -0700

    Add files via upload

---
+++ Winget GUI.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TB_Search = New TextBox()
+        BU_Search = New Button()
+        LB_Search_Out = New ListBox()
+        BU_Add = New Button()
+        LB_HeaderValue = New Label()
+        OFD_Manifest = New OpenFileDialog()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        Label2 = New Label()
+        PictureBox1 = New PictureBox()
+        LB_Manifest = New Label()
+        BU_Remove = New Button()
+        LB_Project = New Label()
+        CB_lnk = New CheckBox()
+        TB_Filename = New TextBox()
+        LB_SelectedID = New ListBox()
+        LB_SelectedName = New ListBox()
+        BU_Build = New Button()
+        MenuStrip1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TB_Search
+        ' 
+        TB_Search.AcceptsReturn = True
+        TB_Search.BorderStyle = BorderStyle.FixedSingle
+        TB_Search.Location = New Point(272, 70)
+        TB_Search.Name = "TB_Search"
+        TB_Search.PlaceholderText = "Search"
+        TB_Search.Size = New Size(416, 23)
+        TB_Search.TabIndex = 0
+        ' 
+        ' BU_Search
+        ' 
+        BU_Search.Location = New Point(426, 99)
+        BU_Search.Name = "BU_Search"
+        BU_Search.Size = New Size(75, 23)
+        BU_Search.TabIndex = 2
+        BU_Search.Text = "&Search"
+        BU_Search.UseVisualStyleBackColor = True
+        ' 
+        ' LB_Search_Out
+        ' 
+        LB_Search_Out.BorderStyle = BorderStyle.FixedSingle
+        LB_Search_Out.FormattingEnabled = True
+        LB_Search_Out.ItemHeight = 15
+        LB_Search_Out.Location = New Point(104, 127)
+        LB_Search_Out.Name = "LB_Search_Out"
+        LB_Search_Out.Size = New Size(739, 182)
+        LB_Search_Out.TabIndex = 3
+        LB_Search_Out.Visible = False
+        ' 
+        ' BU_Add
+        ' 
+        BU_Add.Location = New Point(849, 286)
+        BU_Add.Name = "BU_Add"
+        BU_Add.Size = New Size(75, 23)
+        BU_Add.TabIndex = 4
+        BU_Add.Text = "&Add"
+        BU_Add.UseVisualStyleBackColor = True
+        BU_Add.Visible = False
+        ' 
+        ' LB_HeaderValue
+        ' 
+        LB_HeaderValue.AutoSize = True
+        LB_HeaderValue.Location = New Point(12, 574)
+        LB_HeaderValue.Name = "LB_HeaderValue"
+        LB_HeaderValue.Size = New Size(37, 15)
+        LB_HeaderValue.TabIndex = 7
+        LB_HeaderValue.Text = "          "
+        LB_HeaderValue.Visible = False
+        ' 
+        ' OFD_Manifest
+        ' 
+        OFD_Manifest.FileName = "*.manifest"
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(944, 24)
+        MenuStrip1.TabIndex = 17
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "&File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(103, 22)
+        NewToolStripMenuItem.Text = "&New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(103, 22)
+        OpenToolStripMenuItem.Text = "&Open"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(103, 22)
+        ExitToolStripMenuItem.Text = "&Exit"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(387, 29)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(156, 30)
+        Label2.TabIndex = 18
+        Label2.Text = "Winget Search"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImageLayout = ImageLayout.Stretch
+        PictureBox1.Location = New Point(0, 62)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(960, 540)
+        PictureBox1.TabIndex = 20
+        PictureBox1.TabStop = False
+        ' 
+        ' LB_Manifest
+        ' 
+        LB_Manifest.AutoSize = True
+        LB_Manifest.Location = New Point(23, 320)
+        LB_Manifest.Name = "LB_Manifest"
+        LB_Manifest.Size = New Size(162, 15)
+        LB_Manifest.TabIndex = 35
+        LB_Manifest.Text = "Manifest (Select Items Below)"
+        LB_Manifest.Visible = False
+        ' 
+        ' BU_Remove
+        ' 
+        BU_Remove.Location = New Point(851, 495)
+        BU_Remove.Name = "BU_Remove"
+        BU_Remove.Size = New Size(75, 23)
+        BU_Remove.TabIndex = 34
+        BU_Remove.Text = "&Remove"
+        BU_Remove.UseVisualStyleBackColor = True
+        BU_Remove.Visible = False
+        ' 
+        ' LB_Project
+        ' 
+        LB_Project.AutoSize = True
+        LB_Project.Location = New Point(186, 528)
+        LB_Project.Name = "LB_Project"
+        LB_Project.Size = New Size(79, 15)
+        LB_Project.TabIndex = 33
+        LB_Project.Text = "Project Name"
+        LB_Project.Visible = False
+        ' 
+        ' CB_lnk
+        ' 
+        CB_lnk.AutoSize = True
+        CB_lnk.Location = New Point(186, 575)
+        CB_lnk.Name = "CB_lnk"
+        CB_lnk.Size = New Size(116, 19)
+        CB_lnk.TabIndex = 32
+        CB_lnk.Text = "Cleanup .lnk files"
+        CB_lnk.UseVisualStyleBackColor = True
+        CB_lnk.Visible = False
+        ' 
+        ' TB_Filename
+        ' 
+        TB_Filename.BorderStyle = BorderStyle.FixedSingle
+        TB_Filename.Location = New Point(186, 546)
+        TB_Filename.Name = "TB_Filename"
+        TB_Filename.Size = New Size(535, 23)
+        TB_Filename.TabIndex = 31
+        TB_Filename.Visible = False
+        ' 
+        ' LB_SelectedID
+        ' 
+        LB_SelectedID.BackColor = SystemColors.Control
+        LB_SelectedID.BorderStyle = BorderStyle.None
+        LB_SelectedID.FormattingEnabled = True
+        LB_SelectedID.ItemHeight = 15
+        LB_SelectedID.Location = New Point(643, 338)
+        LB_SelectedID.Name = "LB_SelectedID"
+        LB_SelectedID.SelectionMode = SelectionMode.None
+        LB_SelectedID.Size = New Size(202, 180)
+        LB_SelectedID.TabIndex = 30
+        LB_SelectedID.Visible = False
+        ' 
+        ' LB_SelectedName
+        ' 
+        LB_SelectedName.BackColor = SystemColors.Control
+        LB_SelectedName.BorderStyle = BorderStyle.None
+        LB_SelectedName.FormattingEnabled = True
+        LB_SelectedName.ItemHeight = 15
+        LB_SelectedName.Location = New Point(23, 338)
+        LB_SelectedName.Name = "LB_SelectedName"
+        LB_SelectedName.Size = New Size(592, 180)
+        LB_SelectedName.TabIndex = 29
+        LB_SelectedName.Visible = False
+        ' 
+        ' BU_Build
+        ' 
+        BU_Build.Location = New Point(727, 546)
+        BU_Build.Name = "BU_Build"
+        BU_Build.Size = New Size(75, 23)
+        BU_Build.TabIndex = 28
+        BU_Build.Text = "&Build"
+        BU_Build.UseVisualStyleBackColor = True
+        BU_Build.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(944, 601)
+        Controls.Add(LB_Manifest)
+        Controls.Add(BU_Remove)
+        Controls.Add(LB_Project)
+        Controls.Add(CB_lnk)
+        Controls.Add(TB_Filename)
+        Controls.Add(LB_SelectedID)
+        Controls.Add(LB_SelectedName)
+        Controls.Add(BU_Build)
+        Controls.Add(Label2)
+        Controls.Add(LB_HeaderValue)
+        Controls.Add(BU_Add)
+        Controls.Add(LB_Search_Out)
+        Controls.Add(BU_Search)
+        Controls.Add(TB_Search)
+        Controls.Add(MenuStrip1)
+        Controls.Add(PictureBox1)
+        MaximizeBox = False
+        MaximumSize = New Size(960, 640)
+        MinimumSize = New Size(960, 640)
+        Name = "Form1"
+        Text = "Winget GUI"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TB_Search As TextBox
+    Friend WithEvents BU_Search As Button
+    Friend WithEvents LB_Search_Out As ListBox
+    Friend WithEvents BU_Add As Button
+    Friend WithEvents LB_HeaderValue As Label
+    Friend WithEvents OFD_Manifest As OpenFileDialog
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LB_Manifest As Label
+    Friend WithEvents BU_Remove As Button
+    Friend WithEvents LB_Project As Label
+    Friend WithEvents CB_lnk As CheckBox
+    Friend WithEvents TB_Filename As TextBox
+    Friend WithEvents LB_SelectedID As ListBox
+    Friend WithEvents LB_SelectedName As ListBox
+    Friend WithEvents BU_Build As Button
+End Class
---
+++ Winget GUI.vb
@@ -0,0 +1,330 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+Imports System.Net.WebRequestMethods
+Imports System.Reflection
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Text.Json
+
+Public Class Form1
+    Dim vInternet As String
+    Private Sub BU_Search_Click(sender As Object, e As EventArgs) Handles BU_Search.Click
+        If My.Computer.FileSystem.FileExists("WorkerFile.cmd") = True Then
+            My.Computer.FileSystem.DeleteFile("WorkerFile.cmd")
+        End If
+        If My.Computer.FileSystem.FileExists("WorkerOutput.txt") = True Then
+            My.Computer.FileSystem.DeleteFile("WorkerOutput.txt")
+        End If
+
+        LB_Search_Out.Items.Clear()
+        Dim File As System.IO.StreamWriter
+        File = My.Computer.FileSystem.OpenTextFileWriter("WorkerFile.cmd", True)
+        File.WriteLine(" ")
+        File.WriteLine("@Echo Off")
+        File.WriteLine("CLS")
+        File.WriteLine("Echo =========================================================")
+        File.WriteLine("Echo WorkerFile Process - Search")
+        File.WriteLine("Echo =========================================================")
+        File.WriteLine("WINGET Search """ & TB_Search.Text & """>WorkerOutput.txt")
+        File.Close()
+
+        Dim myProcess As Process = New Process()
+        myProcess.StartInfo.FileName = "cmd.exe"
+        myProcess.StartInfo.Arguments = " /c WorkerFile.cmd"
+        myProcess.StartInfo.UseShellExecute = False
+        myProcess.StartInfo.CreateNoWindow = True
+        myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        myProcess.StartInfo.LoadUserProfile = True
+        myProcess.StartInfo.RedirectStandardError = True
+        myProcess.StartInfo.RedirectStandardInput = True
+        myProcess.StartInfo.RedirectStandardOutput = True
+        myProcess.Start()
+        myProcess.WaitForExit()
+
+        System.Threading.Thread.Sleep(2000)
+
+        'Tidy up output seek for █ and ▒
+        Dim FindLines() As String
+        Dim outputlines As New List(Of String)
+        Dim searchString1 As String = "█"
+        Dim searchString2 As String = "▒"
+        FindLines = System.IO.File.ReadAllLines("WorkerOutput.txt")
+
+        For Each line As String In FindLines
+            If line.Contains(searchString1) = False Then
+                outputlines.Add(line)
+            End If
+            If line.Contains(searchString2) = False Then
+                outputlines.Add(line)
+            End If
+        Next
+
+        'Remove Line 1
+        Dim TxtLines As List(Of String) = System.IO.File.ReadAllLines("WorkerOutput.txt").ToList
+        TxtLines.RemoveAt(0)
+        System.IO.File.WriteAllLines("WorkerOutput.txt", TxtLines)
+
+        'Check for data
+        'No package found matching input criteria.
+        Dim FindText As String = System.IO.File.ReadAllText("WorkerOutput.txt")
+        Dim index As Integer = FindText.IndexOf("No package found matching input criteria.")
+        If index >= 0 Then
+            MsgBox("No package found matching input criteria.")
+            TB_Search.Text = ""
+            LB_Search_Out.Visible = False
+            BU_Add.Visible = False
+        Else
+            'Tidy up list
+            LB_Search_Out.Visible = True
+            BU_Add.Visible = True
+            Dim myfile As String = "WorkerOutput.txt"
+            Dim allLines As String() = System.IO.File.ReadAllLines(myfile)
+
+            For Each line As String In allLines
+                LB_Search_Out.Items.Add(line)
+            Next
+            LB_HeaderValue.Text = LB_Search_Out.Items(0)
+            LB_Search_Out.Items.RemoveAt(0)
+            LB_Search_Out.Items.RemoveAt(0)
+        End If
+        Cleanup()
+    End Sub
+    Private Sub BU_Add_Click(sender As Object, e As EventArgs) Handles BU_Add.Click
+        LB_Manifest.Visible = True
+        BU_Remove.Visible = True
+        CB_lnk.Visible = True
+        LB_Project.Visible = True
+        TB_Filename.Visible = True
+        BU_Build.Visible = True
+        Dim SelectedItem As String = LB_Search_Out.SelectedItem.ToString
+        Dim vName As String
+        Dim vPayload As String
+        Dim vHeader As String
+        vHeader = LB_HeaderValue.Text
+        Dim sIndex = vHeader.IndexOf("I")
+        Dim eIndex = vHeader.IndexOf("V")
+        Dim vLong = eIndex - sIndex
+
+        'Trim out info for Description and Target Payload
+        vPayload = SelectedItem
+        vName = vPayload.Substring(0, sIndex - 1)
+        vPayload = vPayload.Substring(sIndex, vLong)
+        vPayload = vPayload.Replace(" ", "")
+        ' MsgBox("Added " & vName & Chr(13) & "ID: " & vPayload & " to manifest")
+        LB_SelectedName.Items.Add(vName)
+        LB_SelectedID.Items.Add(vPayload)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        If TB_Filename.Text = "" Then
+            MsgBox("No Filename")
+            Exit Sub
+        End If
+        If My.Computer.FileSystem.FileExists(TB_Filename.Text & ".cmd") Then
+            My.Computer.FileSystem.DeleteFile(TB_Filename.Text & ".cmd")
+        End If
+        If My.Computer.FileSystem.FileExists(TB_Filename.Text & ".manifest") Then
+            My.Computer.FileSystem.DeleteFile(TB_Filename.Text & ".manifest")
+        End If
+        If My.Computer.FileSystem.FileExists(TB_Filename.Text & ".ids") Then
+            My.Computer.FileSystem.DeleteFile(TB_Filename.Text & ".ids")
+        End If
+
+
+        Dim File As System.IO.StreamWriter
+        File = My.Computer.FileSystem.OpenTextFileWriter(TB_Filename.Text & ".cmd", True)
+        File.WriteLine(" ")
+        File.WriteLine("@Echo Off")
+        File.WriteLine("CLS")
+        Dim i = 0
+        For Each item As String In LB_SelectedName.Items
+            i = i + 1
+            Dim vPosition As String = LB_SelectedID.Items(i - 1)
+            Dim vPositionName As String = LB_SelectedName.Items(i - 1)
+            File.WriteLine("Echo =========================================================")
+            File.WriteLine("Echo Winget Install - " & vPositionName)
+            File.WriteLine("Echo =========================================================")
+            File.WriteLine("WINGET Install """ & vPosition & """ --accept-package-agreements")
+        Next
+        If CB_lnk.Checked = True Then
+            File.WriteLine("Del C:\Users\Public\*.lnk /Q /S")
+            File.WriteLine("Del %USERS%\Desktop\*.lnk /Q /S")
+        End If
+        File.Close()
+
+        Dim File2 As System.IO.StreamWriter
+        File2 = My.Computer.FileSystem.OpenTextFileWriter(TB_Filename.Text & ".manifest", True)
+        i = 0
+        For Each item As String In LB_SelectedName.Items
+            i = i + 1
+            Dim vPositionName As String = LB_SelectedName.Items(i - 1)
+            File2.WriteLine(vPositionName)
+        Next
+        File2.Close()
+        Dim File3 As System.IO.StreamWriter
+        File3 = My.Computer.FileSystem.OpenTextFileWriter(TB_Filename.Text & ".ids", True)
+        i = 0
+        For Each item As String In LB_SelectedID.Items
+            i = i + 1
+            Dim vPosition As String = LB_SelectedID.Items(i - 1)
+            File3.WriteLine(vPosition)
+        Next
+        File3.Close()
+
+        MsgBox(TB_Filename.Text & ".cmd is ready")
+        Dim path As String = Directory.GetCurrentDirectory()
+        Process.Start("explorer.exe", path)
+        Cleanup()
+        TB_Filename.Text = ""
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Cleanup()
+
+        If My.Computer.Network.Ping("www.bing.com") = "True" Then
+            Dim wc As New WebClient
+            Dim xmlText As String = wc.DownloadString("https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US")
+            System.IO.File.WriteAllText("Wallpaper.xml", xmlText)
+
+            Dim edtConfig = XDocument.Load("Wallpaper.xml")
+
+            Dim vWallpaper As String = <url><%= From ReadEDT In edtConfig.<images>.<image> Select ReadEDT.<url>.Value %></url>
+            Dim FullURL As String = "https://www.bing.com" & vWallpaper
+            DownloadFile(FullURL, "Wallpaper.jpeg")
+
+            Dim id As String = "Wallpaper"
+            Dim folder As String = ""
+            Dim filename As String = System.IO.Path.Combine(folder, id & ".jpeg")
+            Try
+                Using fs As New System.IO.FileStream(filename, IO.FileMode.Open)
+
+                    PictureBox1.Image = New Bitmap(Image.FromStream(fs))
+                    PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+                End Using
+            Catch ex As Exception
+                Dim msg As String = "Filename: " & filename &
+                Environment.NewLine & Environment.NewLine &
+                "Exception: " & ex.ToString
+                MessageBox.Show(msg, "Error Opening Image File")
+            End Try
+        End If
+
+    End Sub
+    Sub Cleanup()
+        If My.Computer.FileSystem.FileExists("WorkerFile.cmd") = True Then
+            My.Computer.FileSystem.DeleteFile("WorkerFile.cmd")
+        End If
+        If My.Computer.FileSystem.FileExists("WorkerOutput.txt") = True Then
+            My.Computer.FileSystem.DeleteFile("WorkerOutput.txt")
+        End If
+        If My.Computer.FileSystem.FileExists("Wallpaper.xml") = True Then
+            My.Computer.FileSystem.DeleteFile("Wallpaper.xml")
+        End If
+        If My.Computer.FileSystem.FileExists("Wallpaper.jpeg") = True Then
+            My.Computer.FileSystem.DeleteFile("Wallpaper.jpeg")
+        End If
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs)
+        Dim listIndex As Integer = LB_SelectedName.SelectedIndex
+
+
+        If listIndex = -1 Then
+            LB_Project.Visible = False
+            TB_Filename.Visible = False
+            BU_Build.Visible = False
+            CB_lnk.Visible = False
+            BU_Remove.Visible = False
+            Exit Sub
+        End If
+        LB_SelectedName.Items.RemoveAt(listIndex)
+        LB_SelectedID.Items.RemoveAt(listIndex)
+
+        If LB_SelectedName.Items.Count = 0 Then
+            LB_Project.Visible = False
+            TB_Filename.Visible = False
+            BU_Build.Visible = False
+            CB_lnk.Visible = False
+            LB_Manifest.Visible = False
+            BU_Remove.Visible = False
+        End If
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        LB_Search_Out.Items.Clear()
+        LB_SelectedName.Items.Clear()
+        LB_SelectedID.Items.Clear()
+        TB_Search.Text = ""
+        OFD_Manifest.Title = "Please Select .manifest File"
+        OFD_Manifest.Filter = "Manifest File|*.manifest"
+        If OFD_Manifest.ShowDialog = DialogResult.OK Then
+
+
+            Dim myfile As String = OFD_Manifest.FileName
+            Dim allLines As String() = System.IO.File.ReadAllLines(myfile)
+            TB_Filename.Text = myfile.Replace(".manifest", "")
+            For Each line As String In allLines
+                LB_SelectedName.Items.Add(line).ToString()
+            Next
+            Dim FileIDs As String = myfile.Replace(".manifest", ".ids")
+            Dim myfile2 As String = FileIDs
+            Dim allLines2 As String() = System.IO.File.ReadAllLines(myfile2)
+            For Each line As String In allLines2
+                LB_SelectedID.Items.Add(line).ToString()
+            Next
+        End If
+        LB_Search_Out.Visible = False
+        BU_Add.Visible = False
+        LB_Project.Visible = True
+        TB_Filename.Visible = True
+        BU_Build.Visible = True
+        BU_Remove.Visible = True
+        LB_Manifest.Visible = True
+        CB_lnk.Visible = True
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+        LB_Manifest.Visible = False
+        BU_Remove.Visible = False
+        LB_Search_Out.Visible = False
+        BU_Add.Visible = False
+        BU_Remove.Visible = False
+        LB_Search_Out.Items.Clear()
+        LB_SelectedName.Items.Clear()
+        LB_SelectedID.Items.Clear()
+        TB_Search.Text = ""
+    End Sub
+    Private Declare Function URLDownloadToFile Lib "urlmon" Alias "URLDownloadToFileA" (ByVal pCaller As Long, ByVal szURL As String, ByVal szFileName As String, ByVal dwReserved As Long, ByVal lpfnCB As Long) As Long
+    Public Function DownloadFile(URL As String, LocalFilename As String) As Boolean
+        DownloadFile = (URLDownloadToFile(0, URL, LocalFilename, 0, 0) = 0)
+    End Function
+
+    Private Sub ScaleImage(ByVal p As PictureBox, ByRef i As Bitmap)
+        If i.Height > p.Height Then
+            Dim diff As Integer = i.Height - p.Height
+            Dim Resized As Bitmap = New Bitmap(i, New Size(i.Width - diff, i.Height - diff))
+            i = Resized
+        End If
+        If i.Width > p.Width Then
+            Dim diff As Integer = i.Width - p.Width
+            Dim Resized As Bitmap = New Bitmap(i, New Size(i.Width - diff, i.Height - diff))
+            i = Resized
+        End If
+
+    End Sub
+
+    Private Sub TB_Search_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TB_Search.KeyPress
+        If e.KeyChar = Chr(13) Then 'Chr(13) is the Enter Key
+            BU_Search_Click(Me, EventArgs.Empty)
+        End If
+        If e.KeyChar = Chr(27) Then
+            TB_Search.Text = ""
+            LB_Search_Out.Items.Clear()
+            LB_Search_Out.Visible = False
+
+        End If
+
+    End Sub
+
+
+
+
+End Class
